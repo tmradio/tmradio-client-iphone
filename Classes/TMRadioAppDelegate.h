14b9668c020889d7d5a4f667fd4dea5b90be9fa7
Alexey Zakhlestin <indeyets@gmail.com>
2011-02-23 00:31:37 +0300

    CoreData models

--- Classes/TMRadioAppDelegate.h
+++ Classes/TMRadioAppDelegate.h
@@ -7,16 +7,29 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
 
 @interface TMRadioAppDelegate : NSObject <UIApplicationDelegate, UITabBarControllerDelegate> {
     UIWindow *window;
     UITabBarController *tabBarController;
 
     BOOL uiIsVisible;
+
+@private
+    NSManagedObjectContext *managedObjectContext_;
+    NSManagedObjectModel *managedObjectModel_;
+    NSPersistentStoreCoordinator *persistentStoreCoordinator_;
 }
 
 @property (nonatomic, retain) IBOutlet UIWindow *window;
 @property (nonatomic, retain) IBOutlet UITabBarController *tabBarController;
 @property (nonatomic) BOOL uiIsVisible;
+
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+ 
+- (NSURL *)applicationDocumentsDirectory;
+- (void)saveContext;
 
 @end
